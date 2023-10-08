6a0ae358809183dc4e1a9f2f3b8bd5e9fee13e58
Cody Fincher <cody.fincher@gmail.com>
2023-10-08 18:01:45 -0500

    feat: consolidate litestar extension imports

--- advanced_alchemy/repository/__init__.py
+++ advanced_alchemy/repository/__init__.py
@@ -1,7 +1,10 @@
 from ._async import SQLAlchemyAsyncRepository
 from ._sync import SQLAlchemySyncRepository
+from ._util import get_instrumented_attr, model_from_dict
 
 __all__ = (
     "SQLAlchemyAsyncRepository",
     "SQLAlchemySyncRepository",
+    "get_instrumented_attr",
+    "model_from_dict",
 )
