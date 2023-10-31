15a7d763e0fcd768303886de148f77cd8b15a1a7
Cody Fincher <204685+cofin@users.noreply.github.com>
2023-10-31 11:07:15 -0500

    feat: enable `flask` support (#86)
    The PR enables Flask support by changing how we validate the session object.
    
    In normal Sessions, the `bind` attribute is never empty. However, when using scoped sessions, this is intended to be empty.  This change will call the `get_bind()` function when the bind is not set.

---
+++ examples/flask.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, sessionmaker
+
+from advanced_alchemy import SQLAlchemySyncRepository
+from advanced_alchemy.base import UUIDBase
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+
+
+class Message(UUIDBase):
+    text: Mapped[str]
+
+
+class MessageRepository(SQLAlchemySyncRepository[Message]):
+    model_type = Message
+
+
+# Working
+with app.app_context():
+    with db.engine.begin() as conn:
+        Message.metadata.create_all(conn)
+
+    session = sessionmaker(db.engine)()
+
+    repo = MessageRepository(session=session)
+    repo.add(Message(text="Hello, world!"))
+
+    message = repo.list()[0]
+    assert message.text == "Hello, world!"  # noqa: S101
+
+
+# Not working
+with app.app_context():
+    with db.engine.begin() as conn:
+        Message.metadata.create_all(conn)
+
+    session = db.session
+    repo = MessageRepository(session=session)
+    repo.add(Message(text="Hello, world!"))
+
+    message = repo.list()[0]
+    assert message.text == "Hello, world!"  # noqa: S101
