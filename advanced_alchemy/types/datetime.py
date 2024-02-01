1f04482560fb72467e6a52ec88290f076046047f
Boseong Choi <31615733+cbscsm@users.noreply.github.com>
2024-02-01 11:09:07 +0900

    Update datetime.py

--- advanced_alchemy/types/datetime.py
+++ advanced_alchemy/types/datetime.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
     from sqlalchemy.engine import Dialect
 
 
-class DateTimeUTC(TypeDecorator[Optional[datetime.datetime]]):
+class DateTimeUTC(TypeDecorator[datetime.datetime]):
     """Timezone Aware DateTime.
 
     Ensure UTC is stored in the database and that TZ aware dates are returned for all dialects.
