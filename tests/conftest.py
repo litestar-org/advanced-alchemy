ed7baddfb489c0457fa45dbb41ef68956626abac
Cody Fincher <cody.fincher@gmail.com>
2024-03-19 17:01:54 -0500

    feat: more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,12 @@
+import asyncio
+from collections.abc import Generator
+
+import pytest
+
 pytest_plugins = ["tests.docker_service_fixtures"]
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator["asyncio.AbstractEventLoop", None, None]:
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    yield loop
+    loop.close()
