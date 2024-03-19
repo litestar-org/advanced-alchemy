6270a6b146e342d38f5a47fe0a807270d02a27c8
Cody Fincher <cody.fincher@gmail.com>
2024-03-19 10:59:58 -0500

    feat: add additional exports for the `litestar` plugin

--- advanced_alchemy/extensions/litestar/__init__.py
+++ advanced_alchemy/extensions/litestar/__init__.py
@@ -1,4 +1,4 @@
-from advanced_alchemy import base, repository, service, types
+from advanced_alchemy import base, mixins, repository, service, types
 from advanced_alchemy.config import AlembicAsyncConfig, AlembicSyncConfig, AsyncSessionConfig, SyncSessionConfig
 
 from .alembic import AlembicCommands
@@ -11,12 +11,21 @@ from .plugins import (
     SQLAlchemySerializationPlugin,
     SQLAlchemySyncConfig,
 )
+from .plugins.init.config.asyncio import autocommit_before_send_handler as async_autocommit_before_send_handler
+from .plugins.init.config.asyncio import default_before_send_handler as async_default_before_send_handler
+from .plugins.init.config.sync import autocommit_before_send_handler as sync_autocommit_before_send_handler
+from .plugins.init.config.sync import default_before_send_handler as sync_default_before_send_handler
 
 __all__ = (
     "base",
     "types",
     "repository",
     "service",
+    "mixins",
+    "sync_autocommit_before_send_handler",
+    "async_autocommit_before_send_handler",
+    "sync_default_before_send_handler",
+    "async_default_before_send_handler",
     "AlembicCommands",
     "AlembicAsyncConfig",
     "AlembicSyncConfig",
