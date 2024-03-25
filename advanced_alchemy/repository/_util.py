8492e6c8d77ec99c7f0ead4b862e0496dbb33596
Peter Schutt <peter.github@proton.me>
2024-03-25 16:42:22 +1000

    fix: moved function breaks Litestar imports.
    `wrap_sqlalchemy_exception()` was moved from `repository._util` to `exceptions`, however it is still imported downstream by litestar from the original location.
    
    This PR adds an alias to the function in `repository._util` for backward compat with litestar.

--- advanced_alchemy/repository/_util.py
+++ advanced_alchemy/repository/_util.py
@@ -2,11 +2,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, cast
 
+from advanced_alchemy.exceptions import wrap_sqlalchemy_exception as _wrap_sqlalchemy_exception
+
 if TYPE_CHECKING:
     from sqlalchemy.orm import InstrumentedAttribute
 
     from advanced_alchemy.base import ModelProtocol
     from advanced_alchemy.repository.typing import ModelT
+
+# NOTE: For backward compatibility with Litestar - this is imported from here within the litestar codebase.
+wrap_sqlalchemy_exception = _wrap_sqlalchemy_exception
 
 
 def get_instrumented_attr(model: type[ModelProtocol], key: str | InstrumentedAttribute) -> InstrumentedAttribute:
