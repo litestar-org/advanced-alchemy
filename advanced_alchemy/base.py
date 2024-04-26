aae7b785ba82d953969b548cab7b8b0dc60cebda
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2024-04-26 16:37:25 +0300

    chore: added noqa

--- advanced_alchemy/base.py
+++ advanced_alchemy/base.py
@@ -219,7 +219,11 @@ class CommonTableAttributes:
             dict[str, Any]: A dict representation of the model
         """
         exclude = {"sa_orm_sentinel", "_sentinel"}.union(self._sa_instance_state.unloaded).union(exclude or [])  # type: ignore[attr-defined]
-        return {field: getattr(self, field) for field in self.__mapper__.columns.keys() if field not in exclude}
+        return {
+            field: getattr(self, field)
+            for field in self.__mapper__.columns.keys()  # noqa: SIM118
+            if field not in exclude
+        }
 
 
 @declarative_mixin
