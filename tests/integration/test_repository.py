a577b9542248217acda67299b2dc851c46dd021f
Cody Fincher <cody.fincher@gmail.com>
2024-05-03 00:59:39 +0000

    fix: test case logic correction

--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -2190,7 +2190,7 @@ async def test_service_upsert_method_match(
     upsert_update_obj = await maybe_async(
         author_service.upsert(data=existing_obj.to_dict(exclude={"id"}), match_fields=["name"]),
     )
-    assert str(upsert_update_obj.id) != str(first_author_id)
+    assert str(upsert_update_obj.id) == str(first_author_id)
     assert upsert_update_obj.name == "Agatha C."
 
     upsert_insert_obj = await maybe_async(
