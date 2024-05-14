efaff96053abcc9e93a8f207a5f9f5ba36c1a0cd
Cody Fincher <cody.fincher@gmail.com>
2024-05-14 18:49:56 +0000

    fix: litestar re-export tests

--- advanced_alchemy/repository/typing.py
+++ advanced_alchemy/repository/typing.py
@@ -4,12 +4,16 @@ if TYPE_CHECKING:
     from sqlalchemy import RowMapping, Select
 
     from advanced_alchemy import base
+    from advanced_alchemy.repository._async import SQLAlchemyAsyncRepository
+    from advanced_alchemy.repository._sync import SQLAlchemySyncRepository
 
 __all__ = (
     "ModelT",
     "SelectT",
     "RowT",
     "MISSING",
+    "SQLAlchemySyncRepositoryT",
+    "SQLAlchemyAsyncRepositoryT",
 )
 
 T = TypeVar("T")
@@ -18,6 +22,8 @@ SelectT = TypeVar("SelectT", bound="Select[Any]")
 RowT = TypeVar("RowT", bound=Tuple[Any, ...])
 RowMappingT = TypeVar("RowMappingT", bound="RowMapping")
 ModelOrRowMappingT = TypeVar("ModelOrRowMappingT", bound="Union[base.ModelProtocol, RowMapping]")
+SQLAlchemySyncRepositoryT = TypeVar("SQLAlchemySyncRepositoryT", bound="SQLAlchemySyncRepository[Any]")
+SQLAlchemyAsyncRepositoryT = TypeVar("SQLAlchemyAsyncRepositoryT", bound="SQLAlchemyAsyncRepository[Any]")
 
 
 class _MISSING:
