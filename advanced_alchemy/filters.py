a6d3258f7d18266d01d4e4dfadcbea53b35c22a8
Cody Fincher <cody.fincher@gmail.com>
2024-06-01 18:06:50 +0000

    wip: filter change

--- advanced_alchemy/filters.py
+++ advanced_alchemy/filters.py
@@ -2,16 +2,19 @@
 
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from collections import abc  # noqa: TCH003
 from dataclasses import dataclass
 from datetime import datetime  # noqa: TCH003
-from typing import TYPE_CHECKING, Any, Generic, Literal, TypeVar
+from typing import TYPE_CHECKING, Any, Generic, Literal, TypeVar, overload
+
+from sqlalchemy import Select, StatementLambdaElement
 
 if TYPE_CHECKING:
     from typing_extensions import TypeAlias
 
 T = TypeVar("T")
-
+ModelT = TypeVar("ModelT")
 __all__ = (
     "BeforeAfter",
     "CollectionFilter",
@@ -25,14 +28,30 @@ __all__ = (
 )
 
 
-FilterTypes: TypeAlias = (
-    "BeforeAfter | OnBeforeAfter | CollectionFilter[Any] | LimitOffset | OrderBy | SearchFilter | NotInCollectionFilter[Any] | NotInSearchFilter"
-)
+FilterTypes: TypeAlias = "BeforeAfter | OnBeforeAfter | CollectionFilter[Any] | LimitOffset | OrderBy | SearchFilter | NotInCollectionFilter[Any] | NotInSearchFilter"
 """Aggregate type alias of the types supported for collection filtering."""
 
 
+class StatementFilter(ABC):
+    @overload
+    @abstractmethod
+    def to_statement(self, statement: Select[tuple[ModelT]]) -> Select[tuple[ModelT]]: ...
+
+    @overload
+    @abstractmethod
+    def to_statement(self, statement: StatementLambdaElement) -> StatementLambdaElement: ...
+
+    @abstractmethod
+    def to_statement(
+        self,
+        statement: Select[tuple[ModelT]] | StatementLambdaElement,
+    ) -> Select[tuple[ModelT]] | StatementLambdaElement:
+        """Add filter to statement"""
+        return statement
+
+
 @dataclass
-class BeforeAfter:
+class BeforeAfter(StatementFilter):
     """Data required to filter a query on a ``datetime`` column."""
 
     field_name: str
