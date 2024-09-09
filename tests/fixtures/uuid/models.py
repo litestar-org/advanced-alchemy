b618f930c1a92be2cf1743571a8e54a3dc3c9f14
winstxnhdw <winstxnhdw@gmail.com>
2024-09-09 20:48:35 +0100

    fix: revert test attempt

--- tests/fixtures/uuid/models.py
+++ tests/fixtures/uuid/models.py
@@ -10,7 +10,6 @@ from sqlalchemy import Column, FetchedValue, ForeignKey, String, Table, func
 from sqlalchemy.orm import Mapped, declared_attr, mapped_column, relationship
 
 from advanced_alchemy.base import (
-    NanoIDBase,
     SlugKey,
     TableArgsType,  # pyright: ignore[reportPrivateUsage]
     UUIDAuditBase,
@@ -95,7 +94,7 @@ uuid_item_tag = Table(
 )
 
 
-class UUIDItem(NanoIDBase):
+class UUIDItem(UUIDBase):
     name: Mapped[str] = mapped_column(String(length=50))  # pyright: ignore
     description: Mapped[str] = mapped_column(String(length=100), nullable=True)  # pyright: ignore
     tags: Mapped[List[UUIDTag]] = relationship(secondary=lambda: uuid_item_tag, back_populates="items")
