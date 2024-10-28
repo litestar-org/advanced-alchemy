74c58da0588ab7e940d325c098eca2fd41ca72e2
Cody Fincher <204685+cofin@users.noreply.github.com>
2024-10-28 10:10:35 -0500

    fix: use `collection.abc.Sequence` instead of `typing.Sequence` (#273)

--- advanced_alchemy/extensions/litestar/plugins/init/plugin.py
+++ advanced_alchemy/extensions/litestar/plugins/init/plugin.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import contextlib
-from typing import TYPE_CHECKING, Any, Sequence, cast
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Any, cast
 
 from litestar.di import Provide
 from litestar.dto import DTOData
@@ -46,6 +47,7 @@ signature_namespace_values: dict[str, Any] = {
     "Parameter": Parameter,
     "Dependency": Dependency,
     "DTOData": DTOData,
+    "Sequence": Sequence,
 }
 
 
