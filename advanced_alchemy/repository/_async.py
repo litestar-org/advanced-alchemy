488711e17cb89b72effe076629f5614b38d5b1fc
Ivan <vanobel159@gmail.com>
2024-12-20 01:42:22 +0300

    fix(typing): fix typing of id_attribute fields (#318)

--- advanced_alchemy/repository/_async.py
+++ advanced_alchemy/repository/_async.py
@@ -65,7 +65,7 @@ POSTGRES_VERSION_SUPPORTING_MERGE: Final = 15
 class SQLAlchemyAsyncRepositoryProtocol(FilterableRepositoryProtocol[ModelT], Protocol[ModelT]):
     """Base Protocol"""
 
-    id_attribute: Any
+    id_attribute: str
     match_fields: list[str] | str | None = None
     statement: Select[tuple[ModelT]]
     session: AsyncSession | async_scoped_session[AsyncSession]
@@ -402,7 +402,7 @@ class SQLAlchemyAsyncRepository(SQLAlchemyAsyncRepositoryProtocol[ModelT], Filte
         :class:`~advanced_alchemy.repository._util.FilterableRepository`
     """
 
-    id_attribute: Any = "id"
+    id_attribute: str = "id"
     """Name of the unique identifier for the model."""
     loader_options: LoadSpec | None = None
     """Default loader options for the repository."""
