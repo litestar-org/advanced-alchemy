8ef099cc7b88ca16ba5852586ca1b138e72982eb
Cody Fincher <cody@litestar.dev>
2024-12-21 23:41:20 +0000

    feat: re-add 3.8 coverage calc

--- advanced_alchemy/__main__.py
+++ advanced_alchemy/__main__.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from advanced_alchemy.cli import add_migration_commands as build_cli_interface
 
 
-def run_cli() -> None:
+def run_cli() -> None:  # pragma: no cover
     """Advanced Alchemy CLI"""
     build_cli_interface()()
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     run_cli()
