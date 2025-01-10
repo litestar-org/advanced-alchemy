1e86056c37409106fd5c6a4ef27e7f610075ec34
Cody Fincher <cody@litestar.dev>
2025-01-10 06:01:19 +0000

    fix: fix `3.9` type hints

--- tests/unit/test_extensions/test_litestar/test_dto_integration.py
+++ tests/unit/test_extensions/test_litestar/test_dto_integration.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from types import ModuleType
-from typing import Any, Callable, Dict, List, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 from uuid import UUID
 
 import pytest
@@ -212,7 +212,7 @@ model_with_func_query = select(ConcreteBase, func_result_query.label("func_resul
 
 class ModelWithFunc(Base):
     __table__ = model_with_func_query
-    func_result: Mapped[int | None] = column_property(model_with_func_query.c.func_result)
+    func_result: Mapped[Optional[int]] = column_property(model_with_func_query.c.func_result)  # noqa: UP007
 
 
 def test_model_using_func() -> None:
