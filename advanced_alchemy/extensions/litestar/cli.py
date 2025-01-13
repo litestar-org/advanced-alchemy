fe441ae37595c6abd1c677846abe9d46408b8b06
Cody Fincher <cody@litestar.dev>
2025-01-13 17:30:14 +0000

    feat: correct fetching location

--- advanced_alchemy/extensions/litestar/cli.py
+++ advanced_alchemy/extensions/litestar/cli.py
@@ -32,7 +32,7 @@ def get_database_migration_plugin(app: Litestar) -> SQLAlchemyInitPlugin:
 @click.group(cls=LitestarGroup, name="database")
 def database_group(ctx: click.Context) -> None:
     """Manage SQLAlchemy database components."""
-    ctx.obj = {"app": ctx.obj, "configs": get_database_migration_plugin(ctx.obj).config}
+    ctx.obj = {"app": ctx.obj, "configs": get_database_migration_plugin(ctx.obj.app).config}
 
 
 add_migration_commands(database_group)
