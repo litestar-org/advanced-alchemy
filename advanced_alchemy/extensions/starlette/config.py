8019990ec9d791db8f8b2e287517370a6be8eff2
Cody Fincher <cody@litestar.dev>
2025-01-26 16:43:04 +0000

    fix: no event handlers.  use lifespan

--- advanced_alchemy/extensions/starlette/config.py
+++ advanced_alchemy/extensions/starlette/config.py
@@ -140,8 +140,6 @@ class SQLAlchemyAsyncConfig(_SQLAlchemyAsyncConfig):
         )
 
         app.add_middleware(BaseHTTPMiddleware, dispatch=self.middleware_dispatch)
-        app.add_event_handler("startup", self.on_startup)  # pyright: ignore[reportUnknownMemberType]
-        app.add_event_handler("shutdown", self.on_shutdown)  # pyright: ignore[reportUnknownMemberType]
 
     async def on_startup(self) -> None:
         """Initialize the Starlette application with this configuration."""
