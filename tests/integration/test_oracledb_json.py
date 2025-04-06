dfc0f7f9232f4f9e85b7ec72fb1436b64432818e
Cody Fincher <204685+cofin@users.noreply.github.com>
2025-04-06 15:29:24 -0500

    feat(tests): fully migrate to `pytest-databases` (#430)
    Migrates all database fixtures to `pytest-database`

--- tests/integration/test_oracledb_json.py
+++ tests/integration/test_oracledb_json.py
@@ -30,9 +30,9 @@ def test_18c_json_constraint_generation(oracle18c_engine: Engine) -> None:
 
 
 @pytest.mark.xdist_group("oracle23")
-def test_23c_json_constraint_generation(oracle23c_engine: Engine) -> None:
-    ddl = str(CreateTable(UUIDEventLog.__table__).compile(oracle23c_engine, dialect=oracle.dialect()))  # type: ignore
+def test_23c_json_constraint_generation(oracle23ai_engine: Engine) -> None:
+    ddl = str(CreateTable(UUIDEventLog.__table__).compile(oracle23ai_engine, dialect=oracle.dialect()))  # type: ignore
     assert "BLOB" in ddl.upper()
     assert "JSON" in ddl.upper()
-    with oracle23c_engine.begin() as conn:
+    with oracle23ai_engine.begin() as conn:
         UUIDEventLog.metadata.create_all(conn)
