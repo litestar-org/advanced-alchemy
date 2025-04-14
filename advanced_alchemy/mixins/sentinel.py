a8795e2160d9f00282c7f176c59b1dd2a32561cc
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-04-14 23:36:09 +0100

    fix: make `SentinelMixin` compatible with `MappedAsDataclass` (#442)
    `MappedAsDataclass` is a mixin introduced in SQLAlchemy 2.0. It introduces massive DX improvements to SQLAlchemy by introducing dataclass type validation to SQLAlchemy models. However, this mixin is incompatible with SQLAlchemy's recommended method of implementing a sentinel column as written in their [documentation](https://docs.sqlalchemy.org/en/20/core/connections.html#configuring-sentinel-columns).
    
    This PR fixes this incompatibility as suggested by the SQLAlchemy maintainer in this [discussion](https://github.com/sqlalchemy/sqlalchemy/discussions/12519#discussioncomment-12804658).

--- advanced_alchemy/mixins/sentinel.py
+++ advanced_alchemy/mixins/sentinel.py
@@ -1,10 +1,35 @@
-from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, orm_insert_sentinel
+from typing import TypedDict
+
+from sqlalchemy.orm import Mapped, MappedAsDataclass, declarative_mixin, declared_attr, mapped_column
+from sqlalchemy.sql.schema import _InsertSentinelColumnDefault  # pyright: ignore [reportPrivateUsage]
+from typing_extensions import NotRequired
+
+
+class SentinelKwargs(TypedDict):
+    init: NotRequired[bool]
 
 
 @declarative_mixin
 class SentinelMixin:
     """Mixin to add a sentinel column for SQLAlchemy models."""
 
+    __abstract__ = True
+
+    _sentinel_kwargs: SentinelKwargs = {}
+
+    def __init_subclass__(cls) -> None:
+        super().__init_subclass__()
+        if issubclass(cls, MappedAsDataclass):
+            cls._sentinel_kwargs["init"] = False
+
     @declared_attr
     def _sentinel(cls) -> Mapped[int]:
-        return orm_insert_sentinel(name="sa_orm_sentinel")
+        return mapped_column(
+            name="sa_orm_sentinel",
+            insert_default=_InsertSentinelColumnDefault(),
+            _omit_from_statements=True,
+            insert_sentinel=True,
+            use_existing_column=True,
+            nullable=True,
+            **cls._sentinel_kwargs,
+        )
