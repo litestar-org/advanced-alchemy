ca2b40ecee4e88865e4cde50dbb0715255cb5b79
winstxnhdw <winston.ho@redbulltechnology.com>
2025-04-14 16:10:09 +0100

    feat: do not use `init` if `MappedAsDataclass` is subclassed

--- advanced_alchemy/mixins/sentinel.py
+++ advanced_alchemy/mixins/sentinel.py
@@ -1,19 +1,31 @@
-from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, mapped_column
-from sqlalchemy.sql.schema import _InsertSentinelColumnDefault as InsertSentinelColumnDefault  # pyright: ignore [reportPrivateUsage]
+from typing import TypedDict
+
+from sqlalchemy.orm import Mapped, MappedAsDataclass, declarative_mixin, declared_attr, mapped_column
+from sqlalchemy.sql.schema import (
+    _InsertSentinelColumnDefault as InsertSentinelColumnDefault,  # pyright: ignore [reportPrivateUsage]
+)
+from typing_extensions import NotRequired
+
+
+class SentinelKwargs(TypedDict):
+    init: NotRequired[bool]
 
 
 @declarative_mixin
 class SentinelMixin:
     """Mixin to add a sentinel column for SQLAlchemy models."""
 
+    def __init_subclass__(cls) -> None:
+        cls.kwargs = SentinelKwargs(init=False) if issubclass(cls, MappedAsDataclass) else SentinelKwargs()
+
     @declared_attr
     def _sentinel(cls) -> Mapped[int]:
         return mapped_column(
-            name='sa_orm_sentinel',
-            init=False,
+            name="sa_orm_sentinel",
             insert_default=InsertSentinelColumnDefault(),
             _omit_from_statements=True,
             insert_sentinel=True,
             use_existing_column=True,
             nullable=True,
+            **cls.kwargs,
         )
