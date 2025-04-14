81d7202815064944f0a42bfd6d5ca17275cc3d45
Cody Fincher <cody@litestar.dev>
2025-04-14 19:29:34 +0000

    fix: enable dataclass support

--- advanced_alchemy/mixins/sentinel.py
+++ advanced_alchemy/mixins/sentinel.py
@@ -1,36 +1,31 @@
 from typing import TypedDict
 
 from sqlalchemy.orm import Mapped, MappedAsDataclass, declarative_mixin, declared_attr, mapped_column
-from sqlalchemy.sql.schema import (
-    _InsertSentinelColumnDefault as InsertSentinelColumnDefault,  # pyright: ignore [reportPrivateUsage]
-)
+from sqlalchemy.sql.schema import _InsertSentinelColumnDefault  # pyright: ignore [reportPrivateUsage]
+from typing_extensions import NotRequired
 
 
-class SentinelKwargs(TypedDict, total=False):
-    init: bool
+class SentinelKwargs(TypedDict):
+    init: NotRequired[bool]
 
 
 @declarative_mixin
 class SentinelMixin:
     """Mixin to add a sentinel column for SQLAlchemy models."""
 
-    kwargs: SentinelKwargs
-
-    def __init_subclass__(cls) -> None:
-        if issubclass(cls, MappedAsDataclass):
-            cls.kwargs = {"init": False}
-
-        else:
-            cls.kwargs = {}
+    __abstract__ = True
 
     @declared_attr
     def _sentinel(cls) -> Mapped[int]:
+        kwargs: SentinelKwargs = {}
+        if issubclass(type[cls], MappedAsDataclass):
+            kwargs["init"] = True
         return mapped_column(
             name="sa_orm_sentinel",
-            insert_default=InsertSentinelColumnDefault(),
+            insert_default=_InsertSentinelColumnDefault(),
             _omit_from_statements=True,
             insert_sentinel=True,
             use_existing_column=True,
             nullable=True,
-            **cls.kwargs,
+            **kwargs,
         )
