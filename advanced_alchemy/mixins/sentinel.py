72222ccd6e74ad237608fbc49bf69b57ec813e25
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-04-14 15:25:22 +0100

    fix: make `SentinelMixin` compatible with `MappedAsDataclass`
    Signed-off-by: Winston H. <56998716+winstxnhdw@users.noreply.github.com>

--- advanced_alchemy/mixins/sentinel.py
+++ advanced_alchemy/mixins/sentinel.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, orm_insert_sentinel
+from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, mapped_column
+from sqlalchemy.sql.schema import _InsertSentinelColumnDefault as InsertSentinelColumnDefault
 
 
 @declarative_mixin
@@ -7,4 +8,12 @@ class SentinelMixin:
 
     @declared_attr
     def _sentinel(cls) -> Mapped[int]:
-        return orm_insert_sentinel(name="sa_orm_sentinel")
+        return mapped_column(
+            name='sa_orm_sentinel',
+            init=False,
+            insert_default=InsertSentinelColumnDefault(),
+            _omit_from_statements=True,
+            insert_sentinel=True,
+            use_existing_column=True,
+            nullable=True,
+        )
