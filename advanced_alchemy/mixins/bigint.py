bc18cb51309d8ebe87a4c8bf3e7fe73de777c8b3
Cody Fincher <204685+cofin@users.noreply.github.com>
2025-05-04 08:21:05 -0500

    fix: BigIntPrimaryKey does not respect schema names (#469)
    BigIntPrimaryKey will now respect schema names.
    
    Fixes #466

--- advanced_alchemy/mixins/bigint.py
+++ advanced_alchemy/mixins/bigint.py
@@ -1,7 +1,24 @@
+from typing import Any, Optional
+
 from sqlalchemy import Sequence
 from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, mapped_column
 
 from advanced_alchemy.types import BigIntIdentity
+
+
+def _get_schema(cls: "BigIntPrimaryKey") -> Optional[str]:  # pragma: nocover
+    """Get the schema for the class if set via __table_args__, __table__, or __table_kwargs__."""
+    table_args = getattr(cls, "__table_args__", None)
+    if isinstance(table_args, dict) and "schema" in table_args:
+        return table_args["schema"]  # type: ignore
+    if isinstance(table_args, tuple) and table_args and isinstance(table_args[-1], dict) and "schema" in table_args[-1]:
+        return table_args[-1]["schema"]  # type: ignore
+    if hasattr(cls, "__table__") and hasattr(cls.__table__, "schema"):  # pyright: ignore
+        return cls.__table__.schema  # type: ignore[no-any-return]
+    table_kwargs = getattr(cls, "__table_kwargs__", None)
+    if isinstance(table_kwargs, dict) and "schema" in table_kwargs:
+        return table_kwargs["schema"]  # type: ignore
+    return None
 
 
 @declarative_mixin
@@ -11,8 +28,11 @@ class BigIntPrimaryKey:
     @declared_attr
     def id(cls) -> Mapped[int]:
         """BigInt Primary key column."""
+        seq_kwargs: dict[str, Any] = {"optional": False}
+        if schema := _get_schema(cls):
+            seq_kwargs["schema"] = schema
         return mapped_column(
             BigIntIdentity,
-            Sequence(f"{cls.__tablename__}_id_seq", optional=False),  # type: ignore[attr-defined]
+            Sequence(f"{cls.__tablename__}_id_seq", **seq_kwargs),  # type: ignore[attr-defined]
             primary_key=True,
         )
