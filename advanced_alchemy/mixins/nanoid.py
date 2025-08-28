2b794b013bbee1f2f2e4820e3e5f52aa5dd25671
Harshal Laheri <harshallaheri12@gmail.com>
2025-08-28 11:29:22 -0400

    chore: add warning message when using uuid instead of nanoid

--- advanced_alchemy/mixins/nanoid.py
+++ advanced_alchemy/mixins/nanoid.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from sqlalchemy.orm import Mapped, declarative_mixin, mapped_column
 
@@ -10,16 +10,20 @@ if NANOID_INSTALLED and not TYPE_CHECKING:
         generate as nanoid,
     )
 else:
-    import logging
     from uuid import uuid4 as nanoid  # type: ignore[assignment,unused-ignore]
-
-    logger = logging.getLogger("advanced_alchemy")
-    logger.warning("`fastnanoid` not installed, falling back to `uuid4` for NanoID generation.")
 
 
 @declarative_mixin
 class NanoIDPrimaryKey(SentinelMixin):
     """Nano ID Primary Key Field Mixin."""
+
+    def __init_subclass__(cls, **kwargs: Any) -> None:
+        super().__init_subclass__(**kwargs)
+        if not NANOID_INSTALLED and not cls.__module__.startswith("advanced_alchemy"):
+            import logging
+
+            logger = logging.getLogger("advanced_alchemy")
+            logger.warning("`fastnanoid` not installed, falling back to `uuid4` for NanoID generation.")
 
     id: Mapped[str] = mapped_column(default=nanoid, primary_key=True)
     """Nano ID Primary key column."""
